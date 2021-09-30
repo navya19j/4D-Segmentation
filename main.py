a52e3a1cc3e5b66250f6f5b12544a91762441f53
Patrick Cleeve <patrickcleeve2@gmail.com>
2021-09-30 12:29:20 +1000

    change main structure

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
+#!/usr/bin/env python3
+
 from subprocess import call
 import os
 
-to_train = input("Do you wish to train the model? (Y/N)?")
+import boundary_box
+import sort
+import newcoord
+import track
+import test
+import train
 
-if (to_train == "Y"):
-    call(["python", "train.py"])
+if __name__ == "__main__":
 
-to_test = input("Do you wish to test on new images? (Y/N)")
+    to_train = input("Do you wish to train the model? (Y/N)?")
 
-if (to_test == "Y"):
+    if (to_train == "Y"):
+        train.main()
+        # call(["python", "train.py"])
 
-    call(["python", "test.py"])
+    to_test = input("Do you wish to test on new images? (Y/N)")
 
-to_track = input("Do you wish to track the segmented images? (Y/N)?")
+    if (to_test == "Y"):
 
-if (to_track == "Y"):
-    root = os.getcwd()
-    dir = input("Enter directory containing masks to be tracked: ")
-    path_bb = os.path.join(root,dir,"bounding_box")
-    path_cbb = os.path.join(root,dir,"complete_bounding_box")
-    path_3d = os.path.join(root,dir,"3D_Box")
+        test.main()
+        # call(["python", "test.py"])
 
-    if (not os.path.isdir(path_bb)):
-        os.mkdir(path_bb)
-    if (not os.path.isdir(path_cbb)):
-        os.mkdir(path_cbb)
-    if (not os.path.isdir(path_3d)):
-        os.mkdir(path_3d)
+    to_track = input("Do you wish to track the segmented images? (Y/N)?")
 
-    print("Generating Bounding Boxes")
-    call(["python", "boundary_box.py","--dir",dir])
-    print("Tracking across z-direction")
-    call(["python", "sort.py","--dir",dir])
-    print("Generating 3D bounding Boxes")
-    call(["python", "newcoord.py","--dir",dir])
-    print("Tracking across time")
-    call(["python", "track.py","--dir",dir])
-    print("Saved final trajectory in object.txt and track.txt")
+    if (to_track == "Y"):
+        root = os.getcwd()
+        dir = input("Enter directory containing masks to be tracked: ")
+        path_bb = os.path.join(root,dir,"bounding_box")
+        path_cbb = os.path.join(root,dir,"complete_bounding_box")
+        path_3d = os.path.join(root,dir,"3D_Box")
 
-    # add visualisation here
+        if (not os.path.isdir(path_bb)):
+            os.mkdir(path_bb)
+        if (not os.path.isdir(path_cbb)):
+            os.mkdir(path_cbb)
+        if (not os.path.isdir(path_3d)):
+            os.mkdir(path_3d)
+
+        # print("Generating Bounding Boxes")
+        # call(["python", "boundary_box.py","--dir",dir])
+        # print("Tracking across z-direction")
+        # call(["python", "sort.py","--dir",dir])
+        # print("Generating 3D bounding Boxes")
+        # call(["python", "newcoord.py","--dir",dir])
+        # print("Tracking across time")
+        # call(["python", "track.py","--dir",dir])
+        
+        boundary_box.create_bound_box(dir)
+        sort.run(dir)
+        newcoord.run(dir)
+        track.run(dir)
+        
+        print("Saved final trajectory in object.txt and track.txt")
+
+        # add visualisation here
 
 
