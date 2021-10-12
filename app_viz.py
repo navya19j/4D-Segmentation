6a18b5776197707b3f09557f79ac4ab9f70266f1
Patrick Cleeve <patrickcleeve2@gmail.com>
2021-10-12 12:54:59 +1100

    fixed image dimensions in viz

--- app_viz.py
+++ app_viz.py
@@ -41,7 +41,7 @@ t_idx = st.slider("Select a timestep", 0, MAX_TIMESTEP)
 
 img, mask, pred = load_images(imgs, masks, preds, t_idx)
 
-MAX_Z_HEIGHT = min(img.shape[2], mask.shape[2], pred.shape[2])
+MAX_Z_HEIGHT = min(img.shape[0], mask.shape[0], pred.shape[0])
 z_idx = st.slider("Select a z-height", 0, MAX_Z_HEIGHT)
 
 button_cols = st.columns(2)
@@ -51,9 +51,9 @@ height_animation_button = button_cols[1].button("Animate Image Over Height")
 height_header = button_cols[1].markdown(f"Z-Height : {z_idx}/{MAX_Z_HEIGHT}")
 
 cols = st.columns(3)
-col_0 = cols[0].image(img[:, :, z_idx], clamp=True, caption="raw image", use_column_width=True)
-col_1 = cols[1].image(mask[:, :, z_idx], clamp=True, caption="label image", use_column_width=True)
-col_2 = cols[2].image(pred[:, :, z_idx], clamp=True, caption="predicted image", use_column_width=True)
+col_0 = cols[0].image(img[z_idx, :, :], clamp=True, caption="raw image", use_column_width=True)
+col_1 = cols[1].image(mask[z_idx, :, :], clamp=True, caption="label image", use_column_width=True)
+col_2 = cols[2].image(pred[z_idx, :, :], clamp=True, caption="predicted image", use_column_width=True)
 
 
 if time_animation_button:
@@ -61,18 +61,18 @@ if time_animation_button:
     for t_idx in range(0, MAX_TIMESTEP):
         img, mask, pred = load_images(imgs, masks, preds, t_idx)
         time_header.markdown(f"Timestep: {t_idx}/{MAX_TIMESTEP}")
-        col_0.image(img[:, :, z_idx], clamp=True, caption="raw image", use_column_width=True)
-        col_1.image(mask[:, :, z_idx], clamp=True, caption="label image", use_column_width=True)
-        col_2.image(pred[:, :, z_idx], clamp=True, caption="predicted image", use_column_width=True)
+        col_0.image(img[z_idx, :, :], clamp=True, caption="raw image", use_column_width=True)
+        col_1.image(mask[z_idx, :, :], clamp=True, caption="label image", use_column_width=True)
+        col_2.image(pred[z_idx, :, :], clamp=True, caption="predicted image", use_column_width=True)
         time.sleep(0.5)
 
 if height_animation_button:
 
     for z_idx in range(MAX_Z_HEIGHT):
         height_header.markdown(f"Z-Height : {z_idx}/{MAX_Z_HEIGHT}")
-        col_0.image(img[:, :, z_idx], clamp=True, caption="raw image", use_column_width=True)
-        col_1.image(mask[:, :, z_idx], clamp=True, caption="label image", use_column_width=True)
-        col_2.image(pred[:, :, z_idx], clamp=True, caption="predicted image", use_column_width=True)
+        col_0.image(img[z_idx, :, :], clamp=True, caption="raw image", use_column_width=True)
+        col_1.image(mask[z_idx, :, :], clamp=True, caption="label image", use_column_width=True)
+        col_2.image(pred[z_idx, :, :], clamp=True, caption="predicted image", use_column_width=True)
         time.sleep(0.2)
 
 
